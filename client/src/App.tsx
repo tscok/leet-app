c85b88869fa1460e71b698b6b23bdb090a4e9e0e
Mikael Carlsson <mikael.carlsson@getflowbox.com>
2022-11-20 15:11:35 +0100

    Adds MUI and client UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import {
+  Alert,
+  Box,
+  CircularProgress,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from '@mui/material'
+
 import useEmployees from './hooks/useEmployees'
+import EmployeeCard from './components/EmployeeCard'
 
 function App() {
   const { data, status } = useEmployees()
 
-  useEffect(() => {
-    console.log({ data, status })
-  }, [data, status])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Stack spacing={2} sx={{ mt: 4 }}>
+        <Typography component="h1" variant="h6">
+          The fellowship of the tretton37
+        </Typography>
+
+        <Paper sx={{ textAlign: 'center', p: 2 }}>
+          <Typography>Potential filter and tools area</Typography>
+        </Paper>
+
+        {status === 'loading' && (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        )}
+
+        {status === 'success' && (
+          <Box>
+            <Grid container spacing={2}>
+              {data.map((employee) => (
+                <Grid key={employee.email} item md={4} sm={6} xs={12}>
+                  <EmployeeCard {...employee} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        )}
+
+        {status === 'error' && (
+          <Alert severity="warning" sx={{ mt: 4 }}>
+            An error occurred while fetching data. This project relies on env vars stored on Heroku,
+            please see https://leet-app.herokuapp.com/
+          </Alert>
+        )}
+      </Stack>
+    </Container>
   )
 }
 
