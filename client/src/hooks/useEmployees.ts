c994b6f4242c2150b75316f2572f73c3993828fe
Mikael Carlsson <mikael.carlsson@getflowbox.com>
2022-11-20 12:26:50 +0100

    Implements endpoint to fetch employees

---
+++ client/src/hooks/useEmployees.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+
+interface Employee {
+  email: string
+  gitHub?: string
+  highlighted: boolean
+  imagePortraitUrl: string
+  imageWallOfLeetUrl: string
+  linkedIn?: string
+  mainText: string
+  manager: string
+  name: string
+  office: string
+  orgUnit: string
+  phoneNumber: string
+  published: boolean
+  stackOverflow?: string
+  twitter?: string
+}
+
+type Status = 'error' | 'loading' | 'success'
+
+export default function useEmployees() {
+  const [data, setData] = useState<Employee[]>([])
+  const [status, setStatus] = useState<Status>('loading')
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response = await fetch('/employees')
+        const { data } = await response.json()
+
+        if (response.status !== 200) {
+          throw new Error(`Fetch failed with status ${response.status}`)
+        }
+
+        setData(data)
+        setStatus('success')
+      } catch (error) {
+        setStatus('error')
+      }
+    })()
+  }, [])
+
+  return { data, status }
+}
